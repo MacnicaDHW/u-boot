5d9528f5d9587cfb2400b0b5c3e9fc3534f43aef
Otavio Salvador <otavio@ossystems.com.br>
2020-04-29 09:48:21 -0300

    ARM: rockchip: evb_mini_rv1108: Fix memory size
    evb_mini_rv1108 is populated with a 512MB Samsung DDR, so fix the
    memory size accordingly.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- board/rockchip/evb_mini_rv1108/evb_mini_rv1108.c
+++ board/rockchip/evb_mini_rv1108/evb_mini_rv1108.c
@@ -49,15 +49,7 @@ int mach_cpu_init(void)
 
 int dram_init(void)
 {
-	gd->ram_size = 0x8000000;
-
-	return 0;
-}
-
-int dram_init_banksize(void)
-{
-	gd->bd->bi_dram[0].start = 0x60000000;
-	gd->bd->bi_dram[0].size = 0x8000000;
+	gd->ram_size = SZ_512M;
 
 	return 0;
 }
